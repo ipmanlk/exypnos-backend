3c616e16fa98d10ffc33ec59cdfda2f77b00f498
ipmanlk <navilk@zoho.com>
2018-10-14 13:24:44 +0530

    improved: api

--- api/exy.php
+++ api/exy.php
@@ -1,16 +1,27 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 
-require_once ('../config/config.php');
+if (isset($_GET['id'])) {
+  require_once ('../config/config.php');
+  $id = $_GET['id'];
+  if ($id == 0) {
+    $stmt = mysqli_prepare($link, "SELECT post_id,title,cover_img,post,card_img1,card_img2,datetime,short_des,description as author_info, uname as author FROM posts,author WHERE post_id > ? AND posts.author_id=author.author_id ORDER BY post_id DESC LIMIT 5");
+  } else {
+    $stmt = mysqli_prepare($link, "SELECT post_id,title,cover_img,post,card_img1,card_img2,datetime,short_des,description as author_info, uname as author FROM posts,author WHERE post_id < ? AND posts.author_id=author.author_id ORDER BY post_id DESC LIMIT 5");
+  }
+  mysqli_stmt_bind_param($stmt, "s", $id);
+  mysqli_stmt_execute($stmt);
+  $result = mysqli_stmt_get_result($stmt);
+  mysqli_stmt_close($stmt);
+  mysqli_close($link);
 
-$getData = mysqli_query($link, "SELECT * FROM author_posts ORDER BY post_id DESC");
-$postData = array();
+  $postData = array();
 
+  while ($row = mysqli_fetch_assoc($result)) {
+    $postData[] = $row;
+  }
 
-while ($row = mysqli_fetch_assoc($getData)) {
-  $postData[] = $row;
+  echo (json_encode($postData));
+
 }
-
-echo (json_encode($postData));
-
-mysqli_close($link);
 ?>
