7bdb9eb80ba6a20e88fcfe047255bd211b10cac0
ipmanlk <navilk@zoho.com>
2018-10-17 20:05:41 +0530

    updated: navBar

--- content/navBar.php
+++ content/navBar.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
-  <a class="navbar-brand" href="#">Exypnos</a>
+  <a class="navbar-brand" href="index.php">Exypnos</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavBar">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse justify-content-between" id="mainNavBar">
     <ul class="navbar-nav mr-aut">
       <li class="nav-item">
-        <a class="nav-link" href="index.php">Add Post</a>
+        <a class="nav-link" href="post.php">Add Post</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="addUser.php">Add User</a>
