5e6c34e64a5b9c7f538198e2f6cb96621486bdb1
ipmanlk <navilk@zoho.com>
2018-10-17 20:54:00 +0530

    add: logout

--- content/navBar.php
+++ content/navBar.php
@@ -5,16 +5,33 @@
   </button>
   <div class="collapse navbar-collapse justify-content-between" id="mainNavBar">
     <ul class="navbar-nav mr-aut">
-      <li class="nav-item">
-        <a class="nav-link" href="post.php?action=add">Add Post</a>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+          Posts
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="./post.php?action=add">Add Post</a>
+          <a class="dropdown-item" href="index.php">Edit Posts</a>
+        </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="addUser.php">Add User</a>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+          Users
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="./addUser.php">Add User</a>
+        </div>
       </li>
+
     </ul>
     <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" href="#"><?php if (isset($_SESSION['username'])) {echo $_SESSION['username'];}  ?></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../tasks/logout.php">Log out</a>
       </li>
     </ul>
   </div>
