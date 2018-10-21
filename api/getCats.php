15f7eb55d6648f28c06390bebdf3975f26ee5cda
ipmanlk <navilk@zoho.com>
2018-10-21 15:35:47 +0530

    add: post count to get category in api

--- api/getCats.php
+++ api/getCats.php
@@ -2,7 +2,7 @@
 // output list of categories
 header('Access-Control-Allow-Origin: *');
 require_once ('../config/config.php');
-$stmt = mysqli_prepare($link, "SELECT * FROM category");
+$stmt = mysqli_prepare($link, "SELECT category.cat_id,name,COUNT(post.cat_id) AS post_num FROM post,category WHERE post.cat_id = category.cat_id GROUP BY name ORDER BY (category.cat_id) ASC");
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
 mysqli_stmt_close($stmt);
