c2b245486f7731d8df1e2e6b4e16b724ac8d2062
ipmanlk <navilk@zoho.com>
2018-10-23 12:03:43 +0530

    add: permission to addPost

--- tasks/addPost.php
+++ tasks/addPost.php
@@ -3,7 +3,7 @@ require_once 'checkSession.php';
 // 1 - data insert successful
 // 2 - inputs are missing
 // 3 - data insert error
-$required = array('title', 'post', 'coverimg', 'cardimg1', 'cardimg2', 'authorid', 'shortdes','categoryid');
+$required = array('title', 'post', 'coverimg', 'cardimg1', 'cardimg2', 'authorid', 'shortdes','categoryid','statusid');
 $values = array();
 //check values are not empty && set properly
 $error = false;
@@ -20,8 +20,8 @@ foreach($required as $field) {
 if (!$error) {
   require_once ('../config/config.php');
   //prepare statement to insert values
-  $stmt = mysqli_prepare($link, "INSERT INTO post(title,post,cover_img,card_img1,card_img2,author_id,short_des,cat_id) VALUES(?,?,?,?,?,?,?,?)");
-  mysqli_stmt_bind_param($stmt,'ssssssss',$title,$post,$coverimg,$cardimg1,$cardimg2,$author,$shortdes,$categoryid);
+  $stmt = mysqli_prepare($link, "INSERT INTO post(title,post,cover_img,card_img1,card_img2,short_des,author_id,cat_id,status_id) VALUES(?,?,?,?,?,?,?,?,?)");
+  mysqli_stmt_bind_param($stmt,'ssssssiii',$title,$post,$coverimg,$cardimg1,$cardimg2,$shortdes,$authorid, $categoryid, $statusid);
 
   //assign values
   $title = $values['title'];
@@ -29,9 +29,14 @@ if (!$error) {
   $coverimg = $values['coverimg'];
   $cardimg1 = $values['cardimg1'];
   $cardimg2 = $values['cardimg2'];
-  $author = $values['authorid'];
+  $authorid = $values['authorid'];
   $shortdes = $values['shortdes'];
   $categoryid = $values['categoryid'];
+  $statusid =  $values['statusid'];
+
+  if (USER_PERMISSION == 3) {
+    $statusid = 2;
+  }
 
   //execute statement & get result
   if (mysqli_stmt_execute($stmt)) {
