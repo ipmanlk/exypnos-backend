39c4f02dc85d59e5832636a6eafc686c5b93290e
ipmanlk <navilk@zoho.com>
2018-10-26 11:01:48 +0530

    change: api file naming

--- api/s.php
+++ api/s.php
@@ -7,19 +7,19 @@ if ($_GET['s']) {
   if (isset($_GET['id']) && isset($_GET['cat_id'])) {
     $id = trim($_GET['id']);
     $cat_id = trim($_GET['cat_id']);
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/cats.php?id=' . $id . "&cat_id=" . $cat_id);
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/postsByCat.php?id=' . $id . "&cat_id=" . $cat_id);
     echo $data;
     exit();
   }
   if (isset($_GET['id'])) {
     $id = trim($_GET['id']);
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/exy.php?id=' . $id);
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/posts.php?id=' . $id);
     echo $data;
     exit();
   }
   if (isset($_GET['sp'])) {
     $id = trim($_GET['id']);
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/getCats.php');
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/catsGet.php');
     echo $data;
   }
 }
