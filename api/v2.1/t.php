e20a03c3b657e2ba4e529cb9385dd72b1b052025
ipmanlk <navilk@zoho.com>
2018-11-06 19:42:07 +0530

    fix: api v2.1 links

--- api/v2.1/t.php
+++ api/v2.1/t.php
@@ -8,13 +8,13 @@ if ($_POST['s']) {
   if (isset($_POST['r'])) {
     // register user
     $reg = trim($_POST['r']);
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2/userAdd.php?r=' . $reg);
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2.1/userAdd.php?r=' . $reg);
     echo $data;
   }
 
   if (isset($_POST['up_c'])) {
     //check version
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2/updateCheck.php');
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2.1/updateCheck.php');
     echo $data;
     exit();
   }
@@ -23,7 +23,7 @@ if ($_POST['s']) {
     //fav add
     $id = $_POST['post_id'];
     $suser_code = $_POST['suser_code'];
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2/favAdd.php?post_id=' . $id . '&suser_code=' . $suser_code);
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2.1/favAdd.php?post_id=' . $id . '&suser_code=' . $suser_code);
     echo $data;
     exit();
   }
@@ -31,7 +31,7 @@ if ($_POST['s']) {
   if (isset($_POST['cmd']) && $_POST['cmd'] == "fav_g") {
     //favs get
     $suser_code = $_POST['suser_code'];
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2/favsGet.php?suser_code=' . $suser_code);
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2.1/favsGet.php?suser_code=' . $suser_code);
     echo $data;
     exit();
   }
@@ -40,7 +40,7 @@ if ($_POST['s']) {
     //fav delete
     $id = $_POST['post_id'];
     $suser_code = $_POST['suser_code'];
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2/favDel.php?post_id=' . $id . '&suser_code=' . $suser_code);
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2.1/favDel.php?post_id=' . $id . '&suser_code=' . $suser_code);
     echo $data;
     exit();
   }
@@ -50,7 +50,7 @@ if ($_POST['s']) {
     //like add
     $id = $_POST['post_id'];
     $suser_code = $_POST['suser_code'];
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2/likeAdd.php?post_id=' . $id . '&suser_code=' . $suser_code);
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2.1/likeAdd.php?post_id=' . $id . '&suser_code=' . $suser_code);
     echo $data;
     exit();
   }
@@ -58,7 +58,7 @@ if ($_POST['s']) {
   if (isset($_POST['lke_g'])) {
     // like get
     $id = trim($_POST['post_id']);
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2/likeGet.php?post_id=' . $id);
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2.1/likeGet.php?post_id=' . $id);
     echo $data;
     exit();
   }
@@ -67,7 +67,7 @@ if ($_POST['s']) {
     // like check
     $id = $_POST['post_id'];
     $suser_code = $_POST['suser_code'];
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2/likeCheck.php?post_id=' . $id . '&suser_code=' . $suser_code);
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2.1/likeCheck.php?post_id=' . $id . '&suser_code=' . $suser_code);
     echo $data;
     exit();
   }
@@ -76,7 +76,7 @@ if ($_POST['s']) {
     // get posts by cat
     $id = trim($_POST['post_id']);
     $cat_id = trim($_POST['cat_id']);
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2/postsByCat.php?id=' . $id . "&cat_id=" . $cat_id);
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2.1/postsByCat.php?id=' . $id . "&cat_id=" . $cat_id);
     echo $data;
     exit();
   }
@@ -84,7 +84,7 @@ if ($_POST['s']) {
   if (isset($_POST['post_id'])  && isset($_POST['p_list'])) {
     // get posts
     $id = trim($_POST['post_id']);
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2/postsList.php?id=' . $id);
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2.1/postsList.php?id=' . $id);
     echo $data;
     exit();
   }
@@ -92,7 +92,7 @@ if ($_POST['s']) {
   if (isset($_POST['post_id'])  && isset($_POST['p_get'])) {
     // get posts
     $id = trim($_POST['post_id']);
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2/postGet.php?id=' . $id);
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2.1/postGet.php?id=' . $id);
     echo $data;
     exit();
   }
@@ -100,7 +100,7 @@ if ($_POST['s']) {
   if (isset($_POST['c_list'])) {
     // get categroies
     $id = trim($_POST['id']);
-    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2/catsGet.php');
+    $data = file_get_contents('https://exypnos.navinda.xyz/api/v2.1/catsGet.php');
     echo $data;
   }
 }
